68960b316d61efa7d5c1503efa48288dce61fa8f
margres <margherita.grespan@gmal.com>
2020-03-30 13:46:05 +0200

    Update Levin.py

--- Levin.py
+++ Levin.py
@@ -7,15 +7,15 @@ import random
 from scipy.special import jv as scipy_besselj
 import sys
 import time
-import scipy.linalg as la
 from mpmath import fp
+
 # $\int e^{ig(x)}f(x)dx$
 # 
 # $\int dx\cdot x J(wxy) \cdot e^{iw[0.5x^2-x+\phi]}dx$
 # 
 # as demonstrated by Levin, we can find an approximation to a less oscillatory solution by collocation.
 
-def diff_eq(F,x,g):
+def differential_eq(F,x,g):
     '''
     input:  F(x), the new not rapidly oscillatory function 
             x, variable in which we need to integrate
@@ -26,7 +26,34 @@ def diff_eq(F,x,g):
     return F.diff(x)+I*g.diff(x)*F
 
 
-def collocation_method(g,f,n_basis):
+def collocation_method(f,n_basis):
+    '''
+    input:  f(x) function in the integral
+            g(x), exponent of the exponential 
+            n_basis, amount of basis funcitons
+            
+    output: the function F(x) which has been approximated using the collocation method.          
+    
+    '''   
+    
+    x = Symbol('x')
+    F = Function('F')(x)
+
+    a_list=[] #list of the constants in the inear combination of n basis funcitons
+    u=[] #basis functions
+
+    for i,c in zip(range(1,n_basis+1),ascii_lowercase):
+        u.append(x**(i-1)) #monomials
+        c = Symbol(str(c))
+        a_list.append(c)
+
+    #print(u)
+    print(a_list)
+    F=np.asarray(a_list).dot(np.asarray(u))
+    return F, a_list
+
+
+def general_collocation_method(A,f,n_basis):
     '''
     input:  f(x) function in the integral
             g(x), exponent of the exponential 
@@ -52,7 +79,7 @@ def collocation_method(g,f,n_basis):
     F=np.asarray(a_list).dot(np.asarray(u))
     #print(F)    
 
-    new_eq=diff_eq(F,x,g)-f
+    new_eq=F.diff(x)+A.transpose()*F-f
     print(new_eq)
     return F,new_eq, a_list
 
@@ -85,7 +112,10 @@ def levin(f,g,lim_inf,lim_sup,n_basis=4):
         #print(x_val)
         
         
-    F,new_eq,variables=collocation_method(g,f,n_basis)
+    F,variables=collocation_method(g,f,n_basis)
+    new_eq=diff_eq(F,x,g)-f
+    print(new_eq)
+    
     equations_list=[]
     for i, x_i in enumerate(x_val):
         equations_list.append(simplify(expand(new_eq.subs({x:x_i}))))
@@ -96,8 +126,8 @@ def levin(f,g,lim_inf,lim_sup,n_basis=4):
     #coefficients= list(fp.lu_solve(A, b), variables).args[0])
     A_n=Matrix(np.hstack((A,b)))
  
-    
     coefficients_LU= solve_linear_system_LU(A_n,variables)
+    
     elapsed_time = time.time() - start_time
     print('Found the coefficient for the non-rapidly-oscillatory f(x) after:', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
     
@@ -114,14 +144,63 @@ def levin(f,g,lim_inf,lim_sup,n_basis=4):
     solution=N(simplify(evaluate.subs({x:lim_sup})-evaluate.subs({x:lim_inf}))) #integral evaluated at the boundaries
     #print(solution)
     return solution
+
+
+def levin_new(f,g,lim_inf,lim_sup,n_basis=4):
+    '''
+    Levin method applied.
+    The collocation points of the basis need to be equidistant.
+    '''
+    start_time = time.time()
+    x = Symbol('x')
+    lim_inf=check_lim(lim_inf)
+    lim_sup=check_lim(lim_sup)
+    
+    #I have to add the check if function has only x as a variable
+    x_val=[]
+    for i in range(n_basis):
+        t=i/n_basis
+        u = 1-t
+        x_val.append(lim_inf*u +  lim_sup*t)
+        #print(x_val)
+        
+    w=([besselj(0, x), besselj(1, x)])  
+    A=([0,-1],[1,-1/x])
+    w_prime=A*w
+    new_eq,variables=general_collocation_method(A,f,n_basis)
+    equations_list=[]
+    for i, x_i in enumerate(x_val):
+        equations_list.append(simplify(expand(new_eq.subs({x:x_i}))))
+
+    A, b = linear_eq_to_matrix(equations_list, variables)
+    elapsed_time = time.time() - start_time
+    print('Created set of linear equations after: ',time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
+    #coefficients= list(fp.lu_solve(A, b), variables).args[0])
+    A_n=Matrix(np.hstack((A,b)))
+ 
+    coefficients_LU= solve_linear_system_LU(A_n,variables)
+    
+    elapsed_time = time.time() - start_time
+    print('Found the coefficient for the non-rapidly-oscillatory f(x) after:', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
+    
+    coefficients=np.zeros(len(variables),dtype=np.complex128)
+    for i,value in enumerate(coefficients_LU.values()):
+        coefficients[i]=expand(value)
+        #print(sol[i])
+        
+    #elapsed_time = time.time() - start_time
+    F_new=F.copy()
+    for coef,coef_val in zip(variables, coefficients):
+        F_new=F_new.subs({coef:coef_val}) 
+    evaluate=F_new*exp(I*g)
+    solution=N(simplify(evaluate.subs({x:lim_sup})-evaluate.subs({x:lim_inf}))) #integral evaluated at the boundaries
+    return solution
+
 '''
 def linsolv(A,b,variables):
      coefficients= list(linsolve((A, b), variables).args[0])
      return coefficients
- 
-def LU(A,b,variables):
-    P, L, _ = la.lu(A)  
-    return P
+
 '''          
 
 if __name__ == "__main__":
@@ -129,12 +208,13 @@ if __name__ == "__main__":
     x = Symbol('x')
     J = besselj(0, x)
     #J_lambda = lambdify(x, J, {'besselj': scipy_besselj})
-    f=x*J
+    f=x
     g=(0.5*x**2-x)
+    w=J*exp(g)
     #for i in range(1,4):
     #    print((i-1)/(4-1))
         
-    print(levin(f,g,0,np.inf,n_basis=19))    
+    print(levin(f,g,0,np.inf,n_basis=7))    
 
 
 
