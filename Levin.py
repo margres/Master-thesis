9e31a333785e6afa5c29a8c975b9f16ca9367e39
margres <margherita.grespan@gmal.com>
2020-04-07 14:02:33 +0200

    Update Levin.py

--- Levin.py
+++ Levin.py
@@ -81,7 +81,7 @@ def levin_basic(f,g,lim_inf,lim_sup,n_basis=4):
     
     #I have to add the check if function has only x as a variable
   
-    x_val=[a+(j-1)*(b-a)/(n-1) for j in range(1,n+1)]
+    x_val=[lim_inf+(j-1)*(lim_sup-lim_inf)/(n_basis-1) for j in range(1,n_basis+1)]
     '''
     for i in range(n_basis):
         t=i/n_basis
@@ -123,15 +123,11 @@ def levin_basic(f,g,lim_inf,lim_sup,n_basis=4):
     #print(solution)
     return solution
 
-def uprime_f(uprime,x_v,k_v):
-    uprime_s=uprime.subs({x:x_v,k:k_v})
-    return uprime_s
-
-def u_f(u,x_v,k_v):
-    u_s=u.subs({x:x_v,k:k_v})
-    return u_s
 
 def Bes(const,x,n_basis,point,order=1):
+    '''
+    function that creates the matrix A for a Bessel function
+    '''
 
     A=Matrix(np.zeros([2*n_basis,2*n_basis]))
     Id=Matrix(np.zeros([2*n_basis,2*n_basis]))
@@ -152,13 +148,16 @@ def coeff_list(n_basis):
     a_list=[Symbol(j) for j in ascii_lowercase[:2*n_basis]]         
     return a_list
 
+def compactification():
+    x=x/(1-x)
+
 def levin_general(f,g,const,lim_inf,lim_sup,n_basis=4):
     '''
     Levin method applied.
     The collocation points of the basis need to be equidistant.
     '''
     #FIND HOW TO DIVIDE X TO THE REST or mmm probably i don't have to
-    
+    start_time = time.time()
     a=lim_inf
     b=lim_sup
     n=n_basis
@@ -176,10 +175,12 @@ def levin_general(f,g,const,lim_inf,lim_sup,n_basis=4):
     
     for i,r in enumerate(point):
         rhs[i]=f.subs({x:r})
+    rhs=Matrix(rhs)
     
     #levin's approximation
     A, Id=Bes(const,x,n_basis,point)
-    A_f=A*u*I*g.diff()+Id*uprime
+    A_f=A*u*I*g.diff()+Id*uprime  #here I create the matrix with the contribute of the Bessel function and the exponential
+    
     
     for j in range(n2):
         if j<n:
@@ -192,25 +193,31 @@ def levin_general(f,g,const,lim_inf,lim_sup,n_basis=4):
             A_f[j,k_i]=A_f[j,k_i].subs({x:val,k:k_})
             #print(j,k_i)
             k_i=0
-    c=coeff_list(n)
-    c=np.hstack((c,c))
-   # rhs=np.array(rhs)
-    rhs=rhs.reshape(n2,1)
-    #print(rhs.shape, A_n.shape)
-    A_n=Matrix(np.hstack((A_f,rhs)))  
-    #print(rhs.shape, A_n.shape)
-    #print(A_n)      
-    coefficients_LU=solve_linear_system_LU(A_n,c)
-    
-    coefficients=np.zeros(len(c),dtype=np.complex128)
-    for i,value in enumerate(coefficients_LU.values()):
-        coefficients[i]=expand(value)
-    
+            
+    if False:       
+        c=coeff_list(n)
+        c=np.hstack((c,c))
+        rhs=rhs.reshape(n2,1)
+        A_n=Matrix(np.hstack((A_f,rhs)))      
+        coefficients_LU=solve_linear_system_LU(A_n,c)
+        
+        coefficients=np.zeros(len(c),dtype=np.complex128)
+        for i,value in enumerate(coefficients_LU.values()):
+            coefficients[i]=expand(value)
+            
+    if True:
+        sol=A_f.LUsolve(rhs)
+        coefficients=np.zeros(n2,dtype=np.complex128)
+        for i,value in enumerate(sol):
+            coefficients[i]=expand(value)
+   
     approx_inf=[u.subs({x:1,k:j}) for j in range(n)]
     approx_sup=[u.subs({x:2,k:j}) for j in range(n)]
   
     result=np.dot(approx_sup,coefficients[:n])-np.dot(approx_inf,coefficients[:n])
     
+    elapsed_time = time.time() - start_time
+    print('Found the coefficient for the non-rapidly-oscillatory f(x) after:', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
     return result
          
 
@@ -224,7 +231,7 @@ if __name__ == "__main__":
     w=J*exp(I*g)
 
         
-    print(levin_general(f,g,1,0.5,1,n_basis=6))    
+    print(levin_general(f,g,1,0.5,1,n_basis=7))    
 
 
 
